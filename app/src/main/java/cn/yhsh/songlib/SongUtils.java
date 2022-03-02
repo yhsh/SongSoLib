32c0ed2bd5b369a79bb1b9d9d257f5597a22315b
xiayiye5 <1722218686@qq.com>
2022-03-02 10:56:57 +0000

    更新so库的引用说明 更新so库的引用说明

--- app/src/main/java/cn/yhsh/songlib/SongUtils.java
+++ app/src/main/java/cn/yhsh/songlib/SongUtils.java
@@ -4,6 +4,8 @@ package cn.yhsh.songlib;
  * @author xiayiye5
  * @date 2022/3/2 11:12
  * 下面的代码不要添加注释，否则可能编译class文件失败
+ * 使用下面的JNI方法注意：
+ * 直接将这个类复制过去，并且路径不能变，现在需要引用so库的项目新建包名cn.yhsh.songlib，然后将SongUtils类复制进去
  */
 public class SongUtils {
     static {
